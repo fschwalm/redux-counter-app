ab689d23d7022cbbcbd40d5e469f61c4bb8af66b
Fagner <fagnerschwalm@gmail.com>
2018-04-18 18:56:18 -0300

    feat: Connect component with the store

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux'
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
-class Counter extends Component {
-    state = {
-        counter: 0
-    }
+class Counter extends React.Component {
 
     counterChangedHandler = ( action, value ) => {
         switch ( action ) {
@@ -28,7 +26,7 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.counter} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +36,10 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        counter: state.counter
+    };
+}
+
+export default connect(mapStateToProps)(Counter);
