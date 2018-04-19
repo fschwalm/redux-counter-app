2811096125889f127686b8d9d8cca2fc6f1abeaf
Fagner <fagnerschwalm@gmail.com>
2018-04-18 23:12:52 -0300

    feat: Store results

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
-import { increment, decrement, add, subtract } from '../../actions';
+import { increment, decrement, add, subtract, storeResult } from '../../actions';
 
 const Counter = props => (
   <div>
@@ -12,12 +12,28 @@ const Counter = props => (
     <CounterControl label="Decrement" clicked={props.onDecrementCounter} />
     <CounterControl label="Add 5" clicked={props.onAddCounter} />
     <CounterControl label="Subtract 5" clicked={props.onSubtractCounter} />
+    <hr />
+    <button onClick={props.onStoreResult}>Store Result</button>
+    <div>
+      {props.storedResults.length === 0 ? (
+        <h3>No results stored</h3>
+      ) : ( 
+        <ul>
+          {props.storedResults.map(result => (
+            <li key={result.id}>
+            { result.value }
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   </div>
 );
 
 const mapStateToProps = state => {
   return {
-    counter: state.counter
+    counter: state.counter,
+    storedResults: state.storedResults
   };
 };
 
@@ -26,7 +42,8 @@ const mapDispatchToProps = dispatch => {
     onIncrementCounter: () => dispatch(increment()),
     onDecrementCounter: () => dispatch(decrement()),
     onAddCounter: () => dispatch(add(5)),
-    onSubtractCounter: () => dispatch(subtract(5))
+    onSubtractCounter: () => dispatch(subtract(5)),
+    onStoreResult: () => dispatch(storeResult())
   };
 };
 
