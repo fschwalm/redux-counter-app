5f7ec6f82f8c40571a77f45ab91514872c200b89
Fagner <fagnerschwalm@gmail.com>
2018-04-21 14:24:07 -0300

    feat: Aplly logger middleware

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from './reducers';
+import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const logger = store => {
+  return next => {
+    return action => {
+      console.log("[Middeware] dispatching", action);
+      const result = next(action);
+      console.log("[Middleware] next state", store.getState());
+      return result;
+    };
+  };
+};
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
